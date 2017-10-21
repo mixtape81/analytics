075bb84c37ebbdd9a3ae823a2868b053c9ff3875
Austin Reuter <abreuter101@gmail.com>
2017-10-20 19:57:22 -0700

    Set-up server

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const PORT = process.env.PORT || 3000;
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+//require db;
+
+app.use(bodyParser.json({limit: '200mb'}));
+app.use(bodyParser.urlencoded({extended: true}));
+
+//('/playlistviews')''
+app.use('/', (req, res) => {
+  console.log('hello');
+});
+
+
+//[{playlist_id: 1}, {playlist_id: 2}]
+
+app.listen(PORT, () => {
+	console.log(`listening on port ${PORT}`);
+});
